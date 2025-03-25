bb79afe28df4029f50a25909da4c3df84b3f16e5
Saket5629 <saket1333.be22@chitkarauniversity.edu.in>
2025-03-25 12:11:17 +0530

    routes index.js

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,12 @@ const app = express()
 app.use(express.urlencoded({ extended : true }))
 app.use(express.json())
 
+// app.use("/api/users", userRoutes);
+// app.use("/api/packages", packageRoutes);
+// app.use("/api/bookings", bookingRoutes);
+// app.use("/api/destinations", destinationRoutes);
+// app.use("/api/reviews", reviewRoutes);
+
 app.listen(process.env.PORT, ()=> {
     console.log(`Server is running at port no ${process.env.PORT}`);
 })
