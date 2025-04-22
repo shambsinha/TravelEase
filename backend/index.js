b3eedb5a0e9a27261a16d1a0e016788031157d9e
Saket5629 <saket1333.be22@chitkarauniversity.edu.in>
2025-04-22 15:52:04 +0530

    cors added

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import databaseConnection from './config/database.js'
+import userRoutes from './routes/userRoutes.js'
+import cors from 'cors';
 dotenv.config({ path: '.env'})
 
 databaseConnection();
 const app = express()
 app.use(express.urlencoded({ extended : true }))
 app.use(express.json())
+const corsOption = {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials: true
+}
+app.use(cors(corsOption));
 
-app.use("/api/users", userRoutes);
+// app.use("/api/users", userRoutes);
 // app.use("/api/packages", packageRoutes);
 // app.use("/api/bookings", bookingRoutes);
 // app.use("/api/destinations", destinationRoutes);
